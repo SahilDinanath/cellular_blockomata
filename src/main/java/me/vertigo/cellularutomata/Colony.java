555bb73cc1afc046d76ac9558fbb5deafc715d91
Sahil Dinanath <sahildinanath@gmail.com>
2023-07-11 21:23:07 +0200

    implementation of Moore's Neighbouring, improved Neighbouring code

--- src/main/java/me/vertigo/cellularutomata/Colony.java
+++ src/main/java/me/vertigo/cellularutomata/Colony.java
@@ -17,124 +17,160 @@ public class Colony {
     //variables
     World world = Bukkit.getWorld("world");
     private Cell[][][] colonyOfCells = new Cell[ySize][xSize][zSize];
-
-    private boolean[][][] futureColony = new boolean[ySize][xSize][zSize];
-
+    private int neighbouringSize;
+    int[][] coordsToCheck = {
+            //von neumann neighbouring
+            {0, 0, -1},
+            {0, 0, +1},
+            {0, -1, 0},
+            {0, +1, 0},
+            {-1, 0, 0},
+            {+1, 0, 0},
+            //moore neighbouring
+            {-1, -1, -1},
+            {-1, -1, 0},
+            {-1, 0, -1},
+            {0, -1, -1},
+            {1, 1, 1},
+            {1, 1, 0},
+            {1, 0, 1},
+            {0, 1, 1},
+            {-1, 1, 1},
+            {1, -1, 1},
+            {1, 1, -1},
+            {-1, 1, 0},
+            {1, -1, 0},
+            {1, 0, -1},
+            {-1, 0, 1},
+            {0, -1, 1},
+            {0, 1, -1},
+            {-1, -1, 1},
+            {-1, 1, -1},
+            {1, -1, -1},
+    };
     //rules of the colony
-    Vector<Range<Integer>> survivalIntervals;
-    Vector<Range<Integer>> spawnIntervals;
-    int states;
+    private Vector<Range<Integer>> survivalIntervals;
+    private Vector<Range<Integer>> spawnIntervals;
+    private int states;
 
-    public Colony(Vector<Range<Integer>> survivalIntervals, Vector<Range<Integer>> spawnIntervals, int states){
+    public Colony(Vector<Range<Integer>> survivalIntervals, Vector<Range<Integer>> spawnIntervals, int states, boolean neighbouringMethod) {
         this.survivalIntervals = survivalIntervals;
         this.spawnIntervals = spawnIntervals;
         this.states = states;
+        //determines whether to use Moore or Von Neumann neighbouring
+        this.neighbouringSize = neighbouringMethod ? 26 : 6;
+        //clears the previous run and spawns the starting blocks
         colonyInit();
-    }
-    public Cell getCellAtCoords(int x,int y,int z){
-        return colonyOfCells[y][x][z];
-    }
-    private void placeCellInWorld(int x, int y, int z) {
-        Block block = world.getBlockAt(x + xCoordsInWorld, y + yCoordsInWorld, z+ zCoordsInWorld);
-        block.setType(getCellAtCoords(x,y,z).getColor());
+
     }
 
-    private void spawnCellAtCoords(Material color,int x,int y, int z){
-        getCellAtCoords(x,y,z).cellSpawn(color,states);
-        placeCellInWorld(x,y,z);
+    public Cell getCellAtCoords(int x, int y, int z) {
+        return colonyOfCells[y][x][z];
     }
-    private void removeCellAtCoords(int x, int y, int z){
+
+    private void placeCellInWorld(int x, int y, int z) {
+        Block block = world.getBlockAt(x + xCoordsInWorld, y + yCoordsInWorld, z + zCoordsInWorld);
+        block.setType(getCellAtCoords(x, y, z).getColor());
+    }
+
+    private void spawnCellAtCoords(Material color, int x, int y, int z) {
+        getCellAtCoords(x, y, z).cellSpawn(color, states);
+        placeCellInWorld(x, y, z);
+    }
+
+    private void removeCellAtCoords(int x, int y, int z) {
         getCellAtCoords(x, y, z).cellKill();
-        placeCellInWorld(x,y,z);
+        placeCellInWorld(x, y, z);
     }
 
     public void spawnRandomCellsInColony() {
         Random rand = new Random();
         int random_number;
         double multiplier = 1.4;
-        int start = (int) multiplier*xSize/4;
-        int end = (int)((3*xSize/4)*(2-multiplier));
-        for (int x =start; x < end; x++) {
+        int start = (int) multiplier * xSize / 4;
+        int end = (int) ((3 * xSize / 4) * (2 - multiplier));
+        for (int x = start; x < end; x++) {
             for (int y = start; y < end; y++) {
                 for (int z = start; z < end; z++) {
                     random_number = rand.nextInt(10);
-                    if(random_number >5){
+                    if (random_number > 5) {
                         spawnCellAtCoords(Material.BLACK_GLAZED_TERRACOTTA, x, y, z);
-                   }
+                    }
                 }
             }
         }
     }
-    public void colonyInit(){
-        for(int x = 0; x< 400;x++){
-            for(int y = 0; y< 400;y++){
-                for(int z = 0; z< 400;z++){
-                    Block block = world.getBlockAt(x + xCoordsInWorld, y + yCoordsInWorld, z+ zCoordsInWorld);
+
+    public void colonyInit() {
+        for (int x = 0; x < 400; x++) {
+            for (int y = 0; y < 400; y++) {
+                for (int z = 0; z < 400; z++) {
+                    Block block = world.getBlockAt(x + xCoordsInWorld, y + yCoordsInWorld, z + zCoordsInWorld);
                     block.setType(Material.AIR);
                 }
             }
         }
-        for(int x = 0; x< xSize;x++){
-            for(int y = 0; y< ySize;y++){
-                for(int z = 0; z< zSize;z++){
+        for (int x = 0; x < xSize; x++) {
+            for (int y = 0; y < ySize; y++) {
+                for (int z = 0; z < zSize; z++) {
                     colonyOfCells[y][x][z] = new Cell();
                 }
             }
         }
         //spawn cube of random blocks in center
 
-        //spawnRandomCellsInColony();
+        spawnRandomCellsInColony();
 
         //spawn single block in center
-        spawnCellAtCoords(Material.RED_STAINED_GLASS,xSize/2,ySize/2,zSize/2);
+        //spawnCellAtCoords(Material.RED_STAINED_GLASS, xSize / 2, ySize / 2, zSize / 2);
     }
-    private boolean checkCellSurvival(int x, int y, int z){
-        int neighbours = getNeighbours(x,y,z);
-        for(Range<Integer> survivalRange : survivalIntervals){
-            if(survivalRange.contains(neighbours)){
-                return true;
-            }
-        }
-        return false;
-    }
-    private boolean checkCellSpawn(int x, int y, int z){
-        int neighbours = getNeighbours(x,y,z);
-        for(Range<Integer> spawnRange: spawnIntervals){
-            if(spawnRange.contains(neighbours)){
+
+    private boolean checkCellSurvival(int x, int y, int z) {
+        int neighbours = getNeighbours(x, y, z);
+        for (Range<Integer> survivalRange : survivalIntervals) {
+            if (survivalRange.contains(neighbours)) {
                 return true;
             }
         }
         return false;
     }
 
-    public void iterateColony(){
-        for(int x = 0; x< xSize;x++) {
+    private boolean checkCellSpawn(int x, int y, int z) {
+        int neighbours = getNeighbours(x, y, z);
+        for (Range<Integer> spawnRange : spawnIntervals) {
+            if (spawnRange.contains(neighbours)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void iterateColony() {
+        for (int x = 0; x < xSize; x++) {
             for (int y = 0; y < ySize; y++) {
                 for (int z = 0; z < zSize; z++) {
-                getCellAtCoords(x,y,z).setSpawnedThisGeneration(false);
+                    getCellAtCoords(x, y, z).setSpawnedThisGeneration(false);
                 }
             }
         }
 
-        for(int x = 0; x< xSize;x++){
-            for(int y = 0; y< ySize;y++){
-                for(int z = 0; z< zSize;z++){
-                    Cell currentCell = getCellAtCoords(x,y,z);
-                    if(currentCell.getAlive()){
-                        if(!checkCellSurvival(x,y,z)){
+        for (int x = 0; x < xSize; x++) {
+            for (int y = 0; y < ySize; y++) {
+                for (int z = 0; z < zSize; z++) {
+                    Cell currentCell = getCellAtCoords(x, y, z);
+                    if (currentCell.getAlive()) {
+                        if (!checkCellSurvival(x, y, z)) {
                             currentCell.setAlive(false);
                         }
-                    }
-                    else{
-                        if(currentCell.getState()>0){
-                            currentCell.setState(currentCell.getState() -1);
-                            if(currentCell.getState() == 0){
-                                removeCellAtCoords(x,y,z);
+                    } else {
+                        if (currentCell.getState() > 0) {
+                            currentCell.setState(currentCell.getState() - 1);
+                            if (currentCell.getState() == 0) {
+                                removeCellAtCoords(x, y, z);
                             }
-                        }
-                        else{
-                            if(checkCellSpawn(x,y,z)){
-                                spawnCellAtCoords(Material.BLACK_GLAZED_TERRACOTTA,x,y,z);
+                        } else {
+                            if (checkCellSpawn(x, y, z)) {
+                                spawnCellAtCoords(Material.BLACK_GLAZED_TERRACOTTA, x, y, z);
                             }
                         }
                     }
@@ -143,52 +179,18 @@ public class Colony {
         }
     }
 
-
-
-    /*public void displayColonyInWorld(){
-        for(int x = 0; x< xSize;x++){
-            for(int y = 0; y< ySize;y++){
-                for(int z = 0; z< zSize;z++){
-                    placeCellInWorld(x, y, z);
-                }
-            }
-        }
-    }*/
-
-    private int getNeighbours(int x, int y, int z){
+    private int getNeighbours(int x, int y, int z) {
         int neighbours = 0;
 
-        if(x > 0){
-            if(getCellAtCoords(x-1,y,z).getState()>0 && !getCellAtCoords(x-1,y,z).getSpawnedThisGeneration()){
-                neighbours++;
-            }
-        }
-        if(x<xSize-1){
-            if(getCellAtCoords(x+1,y,z).getState()>0 && !getCellAtCoords(x+1,y,z).getSpawnedThisGeneration()){
-                neighbours++;
-            }
-        }
-        if(y > 0 ){
-            if(getCellAtCoords(x,y-1,z).getState()>0 && !getCellAtCoords(x,y-1,z).getSpawnedThisGeneration()){
-                neighbours++;
-            }
-        }
-        if(y <ySize-1){
-            if(getCellAtCoords(x,y+1,z).getState()>0 && !getCellAtCoords(x,y+1,z).getSpawnedThisGeneration()){
-                neighbours++;
-            }
-        }
-        if(z > 0 ){
-            if(getCellAtCoords(x,y,z-1).getState()>0 && !getCellAtCoords(x,y,z-1).getSpawnedThisGeneration()){
-                neighbours++;
-            }
-
-        }if(z <zSize-1){
-            if(getCellAtCoords(x,y,z+1).getState()>0  && !getCellAtCoords(x,y,z+1).getSpawnedThisGeneration()){
-                neighbours++;
+        for (int i = 0; i < neighbouringSize; i++) {
+            try {
+                Cell currentCell = getCellAtCoords(x+ coordsToCheck[i][0], y+coordsToCheck[i][1], z+coordsToCheck[i][2]);
+                if (currentCell.getState() > 0 && !currentCell.getSpawnedThisGeneration()) {
+                    neighbours++;
+                }
+            } catch (ArrayIndexOutOfBoundsException ignored) {
             }
         }
         return neighbours;
     }
-
 }
--- src/main/java/me/vertigo/cellularutomata/Main.java
+++ src/main/java/me/vertigo/cellularutomata/Main.java
@@ -8,15 +8,17 @@ import java.util.concurrent.TimeUnit;
 public  class Main {
     //rules
     private Vector<Range<Integer>> survivalInterval = new Vector<>(){{
-        add(Range.closed(0,6));
+        add(Range.closed(4,6));
     }};
     private Vector<Range<Integer>> spawnInterval = new Vector<>(){{
-        add(Range.singleton(1));
         add(Range.singleton(3));
     }};
     private int states = 2;
+
+    //moore = true, von neumann = false
+    private boolean neighbouringMethod = true;
     //colony
-    private Colony colonyOfCells = new Colony(survivalInterval, spawnInterval, states);
+    private Colony colonyOfCells = new Colony(survivalInterval, spawnInterval, states,neighbouringMethod);
     public void runCellularAutomata() {
             colonyOfCells.iterateColony();
     }
