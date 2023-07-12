b0e527cbb7e5aa07039150aa1b972eef90d3aa3a
Sahil Dinanath <sahildinanath@gmail.com>
2023-07-12 15:11:02 +0200

    changed rules

--- src/main/java/me/vertigo/cellularutomata/Main.java
+++ src/main/java/me/vertigo/cellularutomata/Main.java
@@ -8,15 +8,15 @@ import java.util.concurrent.TimeUnit;
 public  class Main {
     //rules
     private Vector<Range<Integer>> survivalInterval = new Vector<>(){{
-        add(Range.closed(4,6));
+        add(Range.closed(5,6));
     }};
     private Vector<Range<Integer>> spawnInterval = new Vector<>(){{
-        add(Range.singleton(3));
+        add(Range.closed(1,3));
     }};
-    private int states = 2;
+    private int states = 7;
 
     //moore = true, von neumann = false
-    private boolean neighbouringMethod = true;
+    private boolean neighbouringMethod = false;
     //colony
     private Colony colonyOfCells = new Colony(survivalInterval, spawnInterval, states,neighbouringMethod);
     public void runCellularAutomata() {
